777cce2762329e4266d99ef5ca55ca0086d24d0e
telekrex <telekrex@gmail.com>
2023-10-14 13:10:28 -0400

    created moment object

--- ideas.md
+++ ideas.md
@@ -1,11 +1,30 @@
 # Format
 
-Scene {
+Scene [Details] {
 	Narrative {
-		List of: [
-		{Hooks: Narrative},
-		{Hooks: Narrative},
-		{Hooks: Narrative}
+		Links [
+		{[Hook]: [Narrative, Narrative]},
+		{[Hook, Hook]: [Narrative]},
+		{[Hook]: [Narrative]}
 		]		
 	}
 }
+
+OR
+
+Moment Object
+Narrative
+Links{[List of hooks]: [List of moments]}
+
+
+
+# Mechanics
+
+User enters text
+Need to sanitize the entry first
+Find fitting hook from action
+Randomly select from list of narratives if more than one
+Use a default hook if no action works
+Select a random hook if action is uncertain
+
+Get it working first, then add inventory/items/clothes/scene stuff.
--- source/wart.py
+++ source/wart.py
@@ -1,12 +1,43 @@
 import os
 
 
-def filetree():
-	for file in os.listdir('../story'):
-		print(file)
+class Moment:
+	def __init__(self, contents):
+	    self.contents = contents
+	    self.label = contents[0].replace('#', '').strip()
+	    self.narra = contents[1]
+	    self.links = contents[3].split(' = ')
+	    self.hooks = self.links[0].split(', ')
+	    self.mmnts = self.links[1].split(', ')
 
 
-filetree()
+def create_object_from_moment(moment):
+	for file in os.listdir('story'):
+		with open(f'story/{file}') as c:
+			lines = c.read().split('\n')
+			start = 0
+			end = 0
+			i = 0
+			for line in lines:
+				if line.startswith('#'):
+					label = line.strip().replace('#', '')
+					if label.strip() == moment.strip():
+						start = i
+				i += 1
+			i = 0
+			for line in lines[start:]:
+				if not line:
+					end = start + i
+					break
+				i += 1
+			return Moment(lines[start:end])
+
+
+mo = create_object_from_moment('nightmare')
+print(mo.label)
+print(mo.narra)
+print(mo.hooks)
+print(mo.mmnts)
 
 
 # def main():
