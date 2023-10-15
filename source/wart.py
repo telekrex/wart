8d4764dd00f1900d8aaa5b25022ba9e430ad4240
telekrex <telekrex@gmail.com>
2023-10-15 09:36:09 -0400

    progress

--- source/wart.py
+++ source/wart.py
@@ -1,101 +1,119 @@
-import os
+#!/usr/bin/env python3
 from random import choice
+import os
 
 
 def sanitize(text):
-	# !
-	# remove symbols / special chars
-	# remove colloquialisms, bits, extras
-	sanitized_text = text.lower().strip()
-	return sanitized_text
+	# remove non alpha-numeric
+	# characters from the text
+	for character in text:
+		if character.isalnum():
+			pass
+		else:
+			text = text.replace(character, '')
+	# then force it to all lowercase
+	# and remove trailing white space
+	text = text.lower().strip()
+	return text
 
 
-
-def find_hook_from_action(action, links):
-
-	# links:
-	# [[[list of hooks], [list of moments]]]
-
-	l = 0
+def choose_from_links(user_choice, links):
+	# look through the each link, where the list
+	# of links looks like this:
+	# links: [[[list of hooks], [list of moments]]]
 	for link in links:
-		
-		# link:
-		# [[list of hooks], [list of moments]]
-		
+		# look through each link, where the link
+		# looks like this:
+		# link: [[list of hooks], [list of moments]]
 		hooks = link[0]
 		moments = link[1]
-
-		# hooks:
-		# [list of hooks]
-
-		# moments:
-		# [list of moments]
-
+		# and hooks, moments look like this:
+		# hooks: [list of hooks]
+		# moments: [list of moments]
 		for hook in hooks:
-			if sanitize(action) == sanitize(hook):
-				# print(links[l])
-
-				choices = moments
-				# print(choices)
-				return choice(choices)
-
-		l += 1
+			# look through this link's hooks,
+			# and if one matches the user choice,
+			# we choose from the corresponding list
+			# of moments from that link
+			if sanitize(user_choice) == sanitize(hook):
+				return choice(moments)
 
 
-
-def get_moment(moment_label):
+def read(moment):
 	# !
 	# this function is massively ugly
 	# so I'll clean it up later, for now
 	# I just need it to do it's job
 	contents = []
-	for file in os.listdir('story'):
-		with open(f'story/{file}') as c:
-			lines = c.read().split('\n')
-			start = 0
-			end = 0
-			i = 0
-			for line in lines:
-				if line.startswith('#'):
-					label = line.strip().replace('#', '')
-					if label.strip() == moment_label.strip():
-						start = i
-				i += 1
-			i = 0
-			for line in lines[start:]:
-				if not line:
-					end = start + i
-					break
-				i += 1
-			contents = lines[start:end]
-	label = contents[0].replace('#', '').strip()
-	narrative = contents[1].strip()
-	links = []
-	for line in contents[3:]:
-		data = line.split(' = ')
-		hooks = data[0].split(', ')
-		moments = data[1].split(', ')
-		links.append([hooks, moments])
-	return label, narrative, links
 
+	try:	
+		for file in os.listdir('story'):
+			with open(f'story/{file}') as c:
+				lines = c.read().split('\n')
+				start = 0
+				end = 0
+				i = 0
+				for line in lines:
+					if line.startswith('#'):
+						label = line.strip().replace('#', '')
+						if label.strip() == moment.strip():
+							start = i
+					i += 1
+				i = 0
+				for line in lines[start:]:
+					if not line:
+						end = start + i
+						break
+					i += 1
+				contents = lines[start:end]
+	except:
+		success = False
+		error = 'Failed to load story content'
+		label = ''
+		narrative = ''
+		links = []
+		return success, error, label, narrative, links
 
-# label, narrative, links = get_moment('awake')
-# print(label)
-# print(narrative)
-# print(links)
-
-# x = find_hook_from_action('sleep', links)
-# print(x)
-
+	try:
+		label = contents[0].replace('#', '').strip()
+		narrative = contents[1].strip()
+		links = []
+		for line in contents[3:]:
+			data = line.split(' = ')
+			hooks = data[0].split(', ')
+			moments = data[1].split(', ')
+			links.append([hooks, moments])
+		success = True
+		error = ''
+		return success, error, label, narrative, links
+	except:
+		success = False
+		error = 'The moment we found had nothing happening.'
+		label = ''
+		narrative = ''
+		links = []
+		return success, error, label, narrative, links
 
 
 def main():
 	pointer = 'awake'
-	while True:
-		moment, narrative, links = get_moment(pointer)
-		print(narrative)
-		action = input("")
-		pointer = find_hook_from_action(action, links)
+	rewind = pointer
+	while True:		
+		success, error, moment, narrative, links = read(pointer)
+		if success:
+			rewind = pointer
+			print(narrative)
+			action = input('')
+			if action:
+				pointer = choose_from_links(action, links)
+			else:
+				# if user provided nothing and hit enter,
+				# just re-deliver the narration and let
+				# them try again.
+				print('Empty!')
+		else:
+			# print(error)
+			pointer = rewind
 
 
 main()
